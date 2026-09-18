b049841d91e751f0dc5c37b2bdee66d4cf094e7d
albinoafonsocumbe <albinoafonsocumbe8687@gmail.com>
2026-09-18 14:34:52 +0200

    Normaliza configuracao de deploy

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,16 +1,19 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const poolConfig = process.env.DATABASE_URL
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl:
+        process.env.NODE_ENV === "production"
+          ? { rejectUnauthorized: false }
+          : false,
     }
-    : {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
+  : {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     };
 
 const pool = new Pool(poolConfig);
